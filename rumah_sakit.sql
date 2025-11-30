c9026c4a645bd429e0eb24d44e8a8276a71345ff
Farhan Mawardi <mawardyfarhan@gmail.com>
2025-11-30 08:42:18 +0700

    Tugas 2

---
+++ rumah_sakit.sql
@@ -0,0 +1,392 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2025 at 08:56 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rumah_sakit`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaksi1` ()   BEGIN
+START TRANSACTION;
+INSERT INTO pasien (id_pasien, nama_pasien, alamat_pasien, jenis_kelamin) 
+VALUES ('5', 'Tio', 'Jl. Sepatu no. 14', 'L');
+SELECT * FROM pasien;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaksi2` ()   BEGIN
+START TRANSACTION;
+INSERT INTO pasien (id_pasien, nama_pasien, alamat_pasien, jenis_kelamin) 
+VALUES ('5', 'Tio', 'Jl. Sepatu no. 14', 'L');
+SELECT * FROM pasien;
+COMMIT;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaksi3` ()   BEGIN
+START TRANSACTION;
+UPDATE pasien
+SET nama_pasien='Agus'
+WHERE id_pasien='3';
+ROLLBACK;
+COMMIT;
+SELECT * FROM pasien;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaksi4` ()   BEGIN
+START TRANSACTION;
+INSERT INTO pasien (id_pasien, nama_pasien, alamat_pasien, jenis_kelamin) 
+VALUES ('6 ', 'Yanti', 'Jl. Kenanga no. 19', 'W');
+ROLLBACK;
+UPDATE pasien
+SET nama_pasien='Agus'
+WHERE id_pasien='3';
+COMMIT;
+SELECT * FROM pasien;
+COMMIT;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `transaksi5` ()   BEGIN
+START TRANSACTION;
+INSERT INTO pasien (id_pasien, nama_pasien, alamat_pasien, jenis_kelamin) 
+VALUES ('6 ', 'Bambang', 'Jl. Kumbang no. 18', 'L');
+SAVEPOINT POINT1;
+
+INSERT INTO pasien (id_pasien, nama_pasien, alamat_pasien, jenis_kelamin) 
+VALUES ('7 ', 'Yanti', 'Jl. Kenanga no. 19', 'W');
+ROLLBACK TO SAVEPOINT POINT1;
+
+INSERT INTO pasien (id_pasien, nama_pasien, alamat_pasien, jenis_kelamin) 
+VALUES ('8 ', 'Joni', 'Jl. Kamboja no. 20', 'L');
+COMMIT;
+SELECT * FROM pasien;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrator`
+--
+
+CREATE TABLE `administrator` (
+  `id_admin` int(3) NOT NULL,
+  `nama_admin` varchar(30) DEFAULT NULL,
+  `waktu_jaga` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar`
+--
+
+CREATE TABLE `daftar` (
+  `id_daftar` int(3) NOT NULL,
+  `id_pasien` int(3) DEFAULT NULL,
+  `id_admin` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id_dokter` int(3) NOT NULL,
+  `nama_dokter` varchar(30) DEFAULT NULL,
+  `alamat_dokter` varchar(100) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `spesialis` varchar(15) DEFAULT NULL,
+  `waktu_kerja` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id_dokter`, `nama_dokter`, `alamat_dokter`, `tanggal_lahir`, `no_hp`, `spesialis`, `waktu_kerja`) VALUES
+(1, 'Hasan', 'Jl. Yos Sudarso No. 6', '1988-06-11', '08123456789', 'THT', 'Selasa, Rabu, & Kamis, Pukul 08.00 - 12.00 WIB'),
+(2, 'Santoso', 'Jl. Pahlawan No. 6', '1990-07-10', '08987654321', 'Bedah', 'Jumat, Sabtu, & Minggu, Pukul 13.00 - 17.00 WIB');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter_admin`
+--
+
+CREATE TABLE `dokter_admin` (
+  `id_data` int(3) NOT NULL,
+  `id_dokter` int(3) DEFAULT NULL,
+  `id_admin` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(6) NOT NULL,
+  `kode_obat` varchar(5) DEFAULT NULL,
+  `nama_obat` varchar(30) DEFAULT NULL,
+  `harga` int(10) DEFAULT NULL,
+  `stok` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `kode_obat`, `nama_obat`, `harga`, `stok`) VALUES
+(1, 'A001', 'Amikacin', 20000, 10),
+(2, 'A002', 'Amoxilin', 15000, 15),
+(3, 'A003', 'Betadine', 7000, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id_pasien` int(3) NOT NULL,
+  `nama_pasien` varchar(30) DEFAULT NULL,
+  `alamat_pasien` varchar(100) DEFAULT NULL,
+  `jenis_kelamin` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id_pasien`, `nama_pasien`, `alamat_pasien`, `jenis_kelamin`) VALUES
+(1, 'Andi Wijaya', 'Jl. Cempaka No. 5', 'L'),
+(2, 'Rina Wati', 'Jl. Melati No. 8', 'P'),
+(3, 'Agus', 'Jl. Anggrek No. 12', 'L'),
+(4, 'Putri Indah', 'Jl. Mawar No. 2', 'P'),
+(5, 'Tio', 'Jl. Sepatu no. 14', 'L'),
+(6, 'Bambang', 'Jl. Kumbang no. 18', 'L'),
+(8, 'Joni', 'Jl. Kamboja no. 20', 'L');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien_dokter`
+--
+
+CREATE TABLE `pasien_dokter` (
+  `id` int(3) NOT NULL,
+  `id_dokter` int(3) DEFAULT NULL,
+  `id_pasien` int(3) DEFAULT NULL,
+  `waktu_periksa` date DEFAULT NULL,
+  `resep` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien_dokter`
+--
+
+INSERT INTO `pasien_dokter` (`id`, `id_dokter`, `id_pasien`, `waktu_periksa`, `resep`) VALUES
+(1, 1, 3, '2024-12-06', NULL),
+(2, 2, 1, '2024-12-06', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_obat`
+--
+
+CREATE TABLE `transaksi_obat` (
+  `id_transaksi` int(3) NOT NULL,
+  `id_pasien` int(3) DEFAULT NULL,
+  `id_obat` int(3) DEFAULT NULL,
+  `jumlah` int(10) DEFAULT NULL,
+  `total_harga` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi_obat`
+--
+
+INSERT INTO `transaksi_obat` (`id_transaksi`, `id_pasien`, `id_obat`, `jumlah`, `total_harga`) VALUES
+(1, 1, 2, 5, 0);
+
+--
+-- Triggers `transaksi_obat`
+--
+DELIMITER $$
+CREATE TRIGGER `tg_jual` AFTER INSERT ON `transaksi_obat` FOR EACH ROW UPDATE obat SET stok = stok-new.jumlah
+WHERE id_obat=new.id_obat
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `daftar`
+--
+ALTER TABLE `daftar`
+  ADD PRIMARY KEY (`id_daftar`),
+  ADD KEY `id_pasien` (`id_pasien`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indexes for table `dokter_admin`
+--
+ALTER TABLE `dokter_admin`
+  ADD PRIMARY KEY (`id_data`),
+  ADD KEY `id_dokter` (`id_dokter`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
+-- Indexes for table `pasien_dokter`
+--
+ALTER TABLE `pasien_dokter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_dokter` (`id_dokter`),
+  ADD KEY `id_pasien` (`id_pasien`);
+
+--
+-- Indexes for table `transaksi_obat`
+--
+ALTER TABLE `transaksi_obat`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_pasien` (`id_pasien`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `id_admin` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `daftar`
+--
+ALTER TABLE `daftar`
+  MODIFY `id_daftar` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id_dokter` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `dokter_admin`
+--
+ALTER TABLE `dokter_admin`
+  MODIFY `id_data` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id_pasien` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `pasien_dokter`
+--
+ALTER TABLE `pasien_dokter`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `transaksi_obat`
+--
+ALTER TABLE `transaksi_obat`
+  MODIFY `id_transaksi` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `daftar`
+--
+ALTER TABLE `daftar`
+  ADD CONSTRAINT `daftar_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `administrator` (`id_admin`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `dokter_admin`
+--
+ALTER TABLE `dokter_admin`
+  ADD CONSTRAINT `dokter_admin_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `administrator` (`id_admin`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `dokter_admin_ibfk_2` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pasien_dokter`
+--
+ALTER TABLE `pasien_dokter`
+  ADD CONSTRAINT `pasien_dokter_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pasien_dokter_ibfk_2` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaksi_obat`
+--
+ALTER TABLE `transaksi_obat`
+  ADD CONSTRAINT `transaksi_obat_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_obat_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
